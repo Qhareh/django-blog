209bea497b246201a6a85e23df0c933b10c32188
@khareh <ngongucynthia@gmail.com>
2022-05-09 12:49:04 +0300

    urls

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Writer:
+    
+    def __init__(self, name1, name2):
+        self.first_name = name1
+        self.last_name = name2 
+
+    def displayBothNames(self):
+        return self.first_name + " " + self.last_name
+
+    def __str__(self):
+        return self.first_name    
+
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
+ 
+from .models import Writer
 
 # Create your views here.
 def home(request):
 
-    context = {}
+    user = Writer("John", "Macharia")
+
+    context = {
+        "first_name": 'cynthia',
+        "last_name": 'khareh',
+        "writer": user,
+        
+    }
+
     return render(request, "index.html", context)
+    
+def blog(request):
+    context = {
+      
+        
+    }
+
+    return render(request, "blog.html", context)
+    
